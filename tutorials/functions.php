969bdec89868b7ae5bcf186237f0cdb95806c915
mdo84 <markus@markusdollinger.de>
2015-05-27 15:40:24 +0200

    funtions added

---
+++ tutorials/functions.php
@@ -0,0 +1,14 @@
+<?php 
+function peng($zahl)  {
+echo "<html>";
+echo "<head>"
+echo "  <title>peng</title>";
+echo "</head>";
+echo "<body>";
+echo $zahl;
+echo "</body>";
+echo "</html>";
+}
+
+peng(23434523459);
+?>
